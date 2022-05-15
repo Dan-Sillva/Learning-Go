f92159413b45b4c52b4bce685a7077a56fa0a13f
Dan <danilo.official22@hotmail.com>
2022-05-15 18:12:38 -0300

    working with iota

--- src/hello.go
+++ src/hello.go
@@ -1,34 +1,28 @@
 package main
 import "fmt"
 
-// exercicio 5:
-
 /*
-Utilizando a solução do exercício anterior:
+Working with iota displaying: 
+    KB: Kilobyte 
+    MB: Megabyte  
+    GB: Gigabyte
+    TB: Terabyte
 
-    Em package-level scope, utilizando a palavra-chave var, crie uma variável com o identificador "y". O tipo desta variável deve ser o tipo subjacente do tipo que você criou no exercício anterior.
-    Na função main:
-        Isto já deve estar feito:
-            Demonstre o valor da variável "x"
-            Demonstre o tipo da variável "x"
-            Atribua 42 à variável "x" utilizando o operador "="
-            Demonstre o valor da variável "x"
-        Agora faça tambem:
-            Utilize conversão para transformar o tipo do valor da variável "x" em seu tipo subjacente e, utilizando o operador "=", atribua o valor de "x" a "y"
-            Demonstre o valor de "y"
-            Demonstre o tipo de "y"
+    => in binary and decimal form 
 */
 
-type best int   
-var y int 
+const (
+    _ = 1 << ( iota * 10 )
+    KB
+    MB
+    GB
+    TB
+)
 
 func main(){
-	var x best
-
-    fmt.Printf("x >> \n   value: %v \t type: %T\n", x, x)
-    x = 42
-    fmt.Println("   value: ", x)
-
-    y = int(x)
-    fmt.Printf("\ny >> \n   value: %v \t type: %T\n", y, y)
+    fmt.Printf("binary\t\t\t\t\t |decimal\n")
+	fmt.Printf("%b\t\t\t\t |%v\n",KB, KB)
+    fmt.Printf("%b\t\t\t |%v\n",MB, MB)
+    fmt.Printf("%b\t\t |%v\n",GB, GB)
+    fmt.Printf("%b|%v\n",TB, TB)
 }
