d4d005758f4c9c542b3bf8df6be7bc8495d28f8e
Dan <danilo.official22@hotmail.com>
2022-05-15 12:27:21 -0300

    Cap3 Ex5

--- src/hello.go
+++ src/hello.go
@@ -1,29 +1,34 @@
 package main
 import "fmt"
 
-// exercicio 4:
+// exercicio 5:
 
 /*
-Crie um tipo. O tipo subjacente deve ser int.
-Crie uma variável para este tipo, com o identificador "x", utilizando a palavra-chave var.
-Na função main:
+Utilizando a solução do exercício anterior:
 
-    Demonstre o valor da variável "x"
-    Demonstre o tipo da variável "x"
-    Atribua 42 à variável "x" utilizando o operador "="
-    Demonstre o valor da variável "x"
-
-Para os aventureiros: https://golang.org/ref/spec#Types
-Agora já somos quase ninjas nível 1!
+    Em package-level scope, utilizando a palavra-chave var, crie uma variável com o identificador "y". O tipo desta variável deve ser o tipo subjacente do tipo que você criou no exercício anterior.
+    Na função main:
+        Isto já deve estar feito:
+            Demonstre o valor da variável "x"
+            Demonstre o tipo da variável "x"
+            Atribua 42 à variável "x" utilizando o operador "="
+            Demonstre o valor da variável "x"
+        Agora faça tambem:
+            Utilize conversão para transformar o tipo do valor da variável "x" em seu tipo subjacente e, utilizando o operador "=", atribua o valor de "x" a "y"
+            Demonstre o valor de "y"
+            Demonstre o tipo de "y"
 */
 
 type best int   
+var y int 
 
 func main(){
 	var x best
 
-    fmt.Printf("value: %v \t type: %T\n", x, x)
+    fmt.Printf("x >> \n   value: %v \t type: %T\n", x, x)
     x = 42
-    fmt.Println("value: ", x)
+    fmt.Println("   value: ", x)
 
+    y = int(x)
+    fmt.Printf("\ny >> \n   value: %v \t type: %T\n", y, y)
 }
