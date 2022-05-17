19a5449d0c4b8e1fc4e8817e5cebc5f4c32bada9
Dan <danilo.official22@hotmail.com>
2022-05-16 22:46:25 -0300

    Cap5 Ex4

--- src/hello.go
+++ src/hello.go
@@ -1,17 +1,25 @@
 package main
 import "fmt"
 
-// exercicio 3
+// exercicio 4
 
 /*
-Crie constantes tipadas e não-tipadas.
-Demonstre seus valores.
+Crie um programa que:
+
+    Atribua um valor int a uma variável
+    Demonstre este valor em decimal, binário e hexadecimal
+    Desloque os bits dessa variável 1 para a esquerda, e atribua o resultado a outra variável
+    Demonstre esta outra variável em decimal, binário e hexadecimal
+
 */
 
-const untip = 10
-const tip int = 10
-
 func main(){
-    fmt.Printf("%T\t%T\n", untip, tip)
+    x := 64
+
+    fmt.Printf("\tdecimal\t\tbinary\t\thexadecimal\n")
+    fmt.Printf("x:\t%v \t\t%b \t%#x\n", x, x, x)
+
+    y := x << 1
+    fmt.Printf("y:\t%v \t\t%b \t%#x\n", y, y, y)
 
 }
