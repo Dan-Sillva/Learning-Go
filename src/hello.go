811d4b99abcdf1c825a85680e0add4287f2b8c4e
Dan-Sillva <danilo.official22@hotmail.com>
2022-05-18 13:16:10 -0300

    Cap7 Ex6

--- src/hello.go
+++ src/hello.go
@@ -1,18 +1,77 @@
 package main;
 import "fmt";
 
-// exercicio 5
+// exercicio 6
 
 /*
-Demonstre o resto da divisão por 4 de todos os números entre 10 e 100
+Crie um programa que demonstre o funcionamento da declaração if.
+
+OBS : eu ia usar funções mas não entendi mt bem como fazia, então deixa pra proxima :D
 */
 
-func main(){
-	fmt.Printf("resto     \t| resultado |\n")
-	fmt.Printf("-=--=--=--=--=--|--=--=--=--|\n")
+const (
+	space = " ";
+	point = "*";
+)
 
-    for x := 10; x <= 20; x++ {
-        y := (x % 4)
-		fmt.Printf("%v / 4:   \t| %v\t    |\n", x, y)
-    };
+func main(){
+	fmt.Printf("\n\n");
+
+	var opt int
+
+	fmt.Printf("Digite um numero: ")
+	fmt.Scanf("%d",&opt);
+
+	fmt.Printf("\n\n");
+
+	if (int(opt) % 2 != 0) {
+		x := 1;
+		y := 1;						// numero de '*'
+		final := (opt - y) / 2;
+
+		for x != 0 {					
+			x = (opt - y) / 2;		// numero de ' '
+
+			for k := 0; k < x; k++ {
+				fmt.Printf(space);
+			}
+	
+			for k := 0; k < y; k++ {
+				fmt.Printf(point);
+			}
+
+			for k := 0; k < x; k++ {
+				fmt.Printf(space);
+			}
+			fmt.Printf("\n");
+			y = y + 2;
+		}
+
+		for x != final {
+			y = y - 2
+			x = (opt - y) / 2;		// numero de ' '
+
+			if y != opt  { 
+				for k := 0; k < x; k++ {
+					fmt.Printf(space);
+				}
+		
+				for k := 0; k < y; k++ {
+					fmt.Printf(point);
+				}
+	
+				for k := 0; k < x; k++ {
+					fmt.Printf(space);
+				}
+				fmt.Printf("\n");
+			}
+			
+		}
+
+	} else {
+		fmt.Printf("o numero nao e um impar\n");
+	}
+
+	fmt.Printf("\n\n");
 }
+
