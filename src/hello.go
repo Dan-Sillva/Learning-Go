cce935840841be3b32a7dc07b61f3f245a2ccfd7
Dan <danilo.official22@hotmail.com>
2022-05-17 23:22:35 -0300

    Cap7 Ex2

--- src/hello.go
+++ src/hello.go
@@ -1,18 +1,29 @@
 package main;
 import "fmt";
 
-// exercicio 1
+// exercicio 2
 
 /*
-Põe na tela: todos os números de 1 a 10000.
+Põe na tela: O unicode code point de todas as letras maiúsculas do alfabeto, três vezes cada.
+Por exemplo: 65 U+0041 'A' U+0041 'A' U+0041 'A' 66 U+0042 'B' U+0042 'B' U+0042 'B' ...e por aí vai.
 */
 
 func main(){
-    x := 1;
-    fmt.Printf("--:\timpar\tpar\n")
+    x := 65;
+    
+    fmt.Printf("\n\n")
+    for y := 0; y < 3; y++ {
+        fmt.Printf("\t\thexa\tunicode")
+    }
+    fmt.Printf("\n\n")
 
-	for x <= 10000 {
-		fmt.Printf("--:\t%v\t%v\n", x, (x + 1));
-		x = x + 2;
+	for x <= 90 {
+        fmt.Printf("| %v value: |",x)
+		for y := 0; y < 3 ; y++{
+            fmt.Printf("\t%#x|\t%#U|", x, x)
+        }
+        fmt.Printf("\n")
+        x++;
 	};
+    fmt.Printf("\n\n")
 }
