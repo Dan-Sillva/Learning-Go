5b6166715370b0b5655732429ab1658a652db703
Dan <danilo.official22@hotmail.com>
2022-05-17 23:46:45 -0300

    Cap7 Ex3

--- src/hello.go
+++ src/hello.go
@@ -1,29 +1,18 @@
 package main;
 import "fmt";
 
-// exercicio 2
+// exercicio 3
 
 /*
-Põe na tela: O unicode code point de todas as letras maiúsculas do alfabeto, três vezes cada.
-Por exemplo: 65 U+0041 'A' U+0041 'A' U+0041 'A' 66 U+0042 'B' U+0042 'B' U+0042 'B' ...e por aí vai.
+Crie um loop utilizando a sintaxe: for condition {}
+Utilize-o para demonstrar os anos desde que você nasceu.
 */
 
 func main(){
-    x := 65;
-    
-    fmt.Printf("\n\n")
-    for y := 0; y < 3; y++ {
-        fmt.Printf("\t\thexa\tunicode")
-    }
-    fmt.Printf("\n\n")
+    x := 2002;
 
-	for x <= 90 {
-        fmt.Printf("| %v value: |",x)
-		for y := 0; y < 3 ; y++{
-            fmt.Printf("\t%#x|\t%#U|", x, x)
-        }
-        fmt.Printf("\n")
+    for x <= 2022 {
+        fmt.Println(x);
         x++;
-	};
-    fmt.Printf("\n\n")
+    };
 }
