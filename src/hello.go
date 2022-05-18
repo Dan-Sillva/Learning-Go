a028307cba792255a8dee4110201d4f105f86024
Dan-Sillva <danilo.official22@hotmail.com>
2022-05-18 08:28:58 -0300

    Cap7 Ex4

--- src/hello.go
+++ src/hello.go
@@ -1,18 +1,21 @@
 package main;
 import "fmt";
 
-// exercicio 3
+// exercicio 4
 
 /*
-Crie um loop utilizando a sintaxe: for condition {}
+Crie um loop utilizando a sintaxe: for {}
 Utilize-o para demonstrar os anos desde que você nasceu.
 */
 
 func main(){
     x := 2002;
 
-    for x <= 2022 {
+    for {
         fmt.Println(x);
         x++;
+		if x > 2022 {
+			break
+		}
     };
 }
